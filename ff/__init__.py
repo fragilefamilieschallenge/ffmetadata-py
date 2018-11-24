47aaec5e577b312e799e554e90ff8463940bc6cf
Vineet Bansal <vineetbansal@gmail.com>
2018-11-23 21:20:45 -0500

    Updated install_requires and bumped version number.

--- ff/__init__.py
+++ ff/__init__.py
@@ -39,7 +39,7 @@ import json
 import urllib
 import requests
 
-__version__ = '1.0.9'
+__version__ = '1.0.10'
 name = "ffmetadata-py"
 BASE_URL = 'http://api.metadata.fragilefamilies.princeton.edu'
 
